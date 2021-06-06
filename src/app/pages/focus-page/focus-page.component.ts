93c1a5b349aad0bf23935c45fe2bc096bc89cca1
Gregorio <gregoriovazya@gmail.com>
2021-06-06 00:15:39 -0600

    Add a funciontal start stop timer

--- src/app/pages/focus-page/focus-page.component.ts
+++ src/app/pages/focus-page/focus-page.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./focus-page.component.css']
 })
 export class FocusPageComponent implements OnInit {
+  public timer: number;
+  public started: boolean;
+  private countInterval: any;
+  public minutes: string;
+  public seconds: string;
 
-  constructor() { }
+  constructor() {
+    this.timer = 1500;
+    this.started = false;
+    this.minutes = '25';
+    this.seconds = '00';
+    this.countInterval = null;
+  }
 
   ngOnInit(): void {
+  }
+
+  startTimer() {
+    if (!this.started) {
+      this.started = !this.started;
+      this.countInterval = setInterval(this._controls, 1000);
+    } else {
+      this.started = !this.started;
+      clearInterval(this.countInterval);
+    }
+  }
+
+  private _controls = () => {
+    this.timer = this.timer - 1;
+    let minute = Math.floor(this.timer / 60);
+    let second = Math.floor(this.timer % 60);
+    this.minutes = minute.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    this.seconds = second.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
   }
 
 }
